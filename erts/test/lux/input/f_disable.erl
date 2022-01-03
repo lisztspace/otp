e97b581dee0447c927479c45ba8125894862d806
Cons T Åhs <cons@erlang.org>
2022-01-03 19:50:16 +0100

    [features, test] Add new tests for disabling features

---
+++ erts/test/lux/input/f_disable.erl
@@ -0,0 +1,54 @@
+%%
+%% %CopyrightBegin%
+%%
+%% Copyright Ericsson AB 2021-2022. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+%% %CopyrightEnd%
+%%
+
+-module(f_disable).
+
+-export([do/1,
+	 no_ifn/0,
+	 no_ftrs/0]).
+
+-compile({disable_feature, ifn_expr}).
+
+-if(?FEATURE_ENABLED(ifn_expr)).
+-define(FOO, has_ifn).
+-else.
+-define(FOO, no_ifn).
+-endif.
+
+no_ifn() ->
+    [ifn, 'maybe', ?FOO].
+
+-compile({disable_feature, maybe_expr}).
+
+-if(?FEATURE_ENABLED(maybe_expr)).
+-define(BAR, has_maybe).
+-else.
+-define(BAR, no_maybe).
+-endif.
+
+no_ftrs() ->
+    [ifn, maybe, then, ?BAR].
+
+-compile({enable_feature, ifn_expr}).
+
+do(X) ->
+    ifn X > 10 ->
+	    maybe
+    end.
